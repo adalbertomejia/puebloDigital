37f987f9a40db28b3f4de333d4b1c674c7a78856
Adalberto <64305390+adalbertomejia@users.noreply.github.com>
2026-05-18 08:11:54 -0600

    Revert "Agregar panel operativo de tomas de agua y vistas por rol"

--- apps/agua/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from apps.agua.views import toma_list_view
-
-app_name = 'agua'
-
-urlpatterns = [
-    path('tomas/', toma_list_view, name='toma_list'),
-]
--- apps/core/views.py
+++ apps/core/views.py
@@ -1,15 +1,4 @@
 from django.shortcuts import render
 
-from apps.comites.models import UsuarioApp
-
-
 def home(request):
-    perfiles = []
-    if request.user.is_authenticated:
-        perfiles = (
-            UsuarioApp.objects.filter(user=request.user, activo=True)
-            .select_related('comite')
-            .order_by('comite__nombre', 'rol')
-        )
-
-    return render(request, 'home.html', {'perfiles': perfiles})
+    return render(request, 'home.html')
